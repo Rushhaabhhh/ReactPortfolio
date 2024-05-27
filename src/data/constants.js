3b7394b0b8c0d7116a5529eb1781da349aaf631f
Rushabh <145254552+Rushhaabhhh@users.noreply.github.com>
2024-05-27 22:40:53 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,329 @@
+export const Bio = {
+  name: "Rushabh Mistry",
+  roles: [
+    "Student",
+    "Meta Developer",
+    "UI/UX Designer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Rushhaabhhh",
+  linkedin: "linkedin.com/in/rushabhmistry1610",
+  email : "rushabhmistry16@gmail.com",
+  resume:
+    "https://drive.google.com/file/d/1RmdXHmFWn2WatfcMZaP5oW_xlTvD5qg6/view?usp=drivesdk",
+  twitter: "https://twitter.com/Rushaabhh",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name : "Unity",
+        image : "",
+      }
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "",
+    role: "Trimester 1",
+    company: "Scaler School of Technology",
+    date: "Aug 2023 - Oct 2023",
+    desc: "I have completed the first trimester and have learned Java Basics, Data Structures, Computer Science, Command Line Interface, Shell Scripting, and Discrete Maths.",
+    skills: [
+      "Java Basics",
+      "Data Structures",
+      "Computer Science",
+      "Command Line Interface",
+      "Shell Scripting",
+      "Discrete Maths"
+    ],
+  },
+  {
+    id: 0,
+    img : "",
+    role: "Trimester 2",
+    company: "Scaler School of Technology",
+    date: "Nov 2023 - Jan 2024",
+    desc: "Exploring advanced topics of data structures and algorithms, frontend development. Working on projects and assignments to apply what I have learned in real-world scenarios.",
+    skills: [
+      "Advanced Data Structures",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Linear Algebra",
+    ],
+  },
+  {
+    id: 1,
+    img: "",
+    role: "Trimester 3",
+    company: "Scaler School of Technology",
+    date: "Feb 2024 - May 2024",
+    desc: " I will be learning advanced topics in data structures and algorithms, backend development, and system design. I will also be working on projects and assignments to apply what I have learned in real-world scenarios.",
+    skills: [
+      "Advanced Data Structures and Algorithms",
+      "ReactJS",
+      "NodeJs",
+      "SpringBoot",
+      "Probability and Statistics",
+    ],
+  },
+  {
+    id: 2,
+    img: "",
+    role : "Project Intern",
+    company: "Urban Company",
+    date: "May 2024 - Present",
+    desc: "I will be working as a project intern at Urban Company, where I will be working on real-world projects and gaining hands-on experience in software development. I will be working on projects that will help me apply what I have learned in the Scaler School of Technology in real-world scenarios.",
+    skills: [
+      "Unity",
+      "C#",
+      "Game Development",
+      "Game Design"
+    ],
+  },
+  {
+    id: 3,
+    img: "",
+    role: "Trimester 4",
+    company: "Scaler School of Technology",
+    date: "May 2024 - Present",
+    desc: "I will be learning advanced topics in data structures and algorithms, backend development, and system design. I will also be working on projects and assignments to apply what I have learned in real-world scenarios.",
+    skills: [
+      "Advanced Data Structures and Algorithms",
+      "ReactJS",
+      "NodeJs",
+      "DBMS",
+      "Operating Systems",
+      "MVC and CRUD Operations",
+
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "",
+    school: "Scaler School of Technology",
+    date: "Aug 2023 - Aug 2027",
+    grade: "7.5 CGPA",
+    desc: "I am currently pursuing a Bachelor of Technology in Computer Science and Engineering at Scaler School of Technology. I am learning advanced topics in data structures and algorithms, frontend development, backend development, and system design. I am also working on projects and assignments to apply what I have learned in real-world scenarios.",
+    degree: "",
+  },
+  {
+    id: 1,
+    img: "",
+    school: "BITS Pilani",
+    date: "Aug 2023 - Aug 2026",
+    grade: "88.2%",
+    desc: "Pursuing a Bachelor of Technology in Computer Science and Engineering at BITS Pilani. I am learning advanced topics in data structures and algorithms, frontend development, backend development, and system design.",
+    degree: "B.Sc. in Computer Science and Engineering",
+  },
+  {
+    id: 2,
+    img: "",
+    school: "Ryan International School",
+    date: "Apr 2015 - Apr 2022",
+    grade: "90.5%",
+    desc: " Completed my schooling at Ryan International School. I studied Science with Computer Science as my major subjects. I was actively involved in extracurricular activities and was part of the school's football team.",
+    degree: "CBSE(XII), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Portfolio Page",
+    date: "Nov 2023",
+    description:
+      "Basic Portfolio Page made with HTML, CSS. It has a responsive design and is mobile-friendly.",
+    image:
+      "",
+    tags: [
+      "HTML",
+      "CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/Rushhaabhhh/Web-Dev-Hackathon",
+    webapp: "https://rushhaabhhh.github.io/Web-Dev-Hackathon/",
+  },
+  {
+    id: 1,
+    title: "Music Player",
+    date: "Feb 2024",
+    description:
+      " A simple music player made with HTML, CSS, and JavaScript. It has a play, pause, and stop button. It also has a volume control slider. It has a responsive design and is mobile-friendly.",
+    image:
+      "",
+    tags: ["HTML", "CSS", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/Rushhaabhhh/Music-Player",
+    webapp: "https://music-player-ql0ep0nid-rushabhs-projects-dd33ff8c.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Cart Service",
+    date: "April 2024",
+    description:
+      " A simple cart service made with Java, Spring Boot, and MySQL. It has a REST API to add, update, delete, and get cart items.",
+    image:
+      "",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "MySQL",
+      "REST API",
+    ],
+    category: "springboot",
+    github: "https://github.com/Rushhaabhhh/CartService-Springboot",
+  },
+  {
+    id: 3,
+    title: "Tic-Tac-Toe Game",
+    date: "April 2024",
+    description:
+      " A simple Tic-Tac-Toe game made with Java and Spring Boot. It has a REST API to play the game.",
+    image:
+      "",
+    tags: ["Java", "Spring Boot"],
+    category: "springboot",
+    github: "https://github.com/Rushhaabhhh/Tic-Tac-Toe",
+  },
+  {
+    id: 4,
+    title: "Product Service",
+    date: "May 2024",
+    description:
+      " A simple product service made with Java, Spring Boot, and MySQL. It has a REST API to add, update, delete, and get products.",
+    image:
+      "",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "MySQL",
+      "REST API",
+    ],
+    category: "springboot",
+    github: "https://github.com/Rushhaabhhh/ProductService",
+  },
+  {
+    id: 5,
+    title: "Multi Stepper Form",
+    date: "MArch 2024",
+    description:
+      " A Multi Stepper Form made with React JS, HTML, CSS, and JavaScript. It has a responsive design and is mobile-friendly. It has a stepper where users can fill in their details. It has a summary page where users can see all the details they have filled in. It has a confirmation page where users can confirm their details. It has a success page where users can see a success message.",
+    image: "",
+    tags: ["HTML", "Tailwind CSS", "JavaScript", "React"],
+    category: "web app",
+    github: "https://github.com/Rushhaabhhh/Multi-Stepper-Form",
+    webapp: "https://multi-stepper-form-o9ao4wgkl-rushabhs-projects-dd33ff8c.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "WallCraft TRD",
+    date: "May 2024",
+    description:
+      "This document outlines the technical approach, algorithm, and implementation plan for the development of WallCraft, an app designed to detect walls in a room and apply selected wall panel designs using the Meta Quest SDK. The goal is to offer users a seamless experience in customizing their wall panels while ensuring they do not obstruct any objects in the room.",
+    image:
+      "",
+    tags: ["Unity", "C#", "Oculus SDK", "OpenXR", "TensorFlow"],
+    category: "web app",
+    github: "https://github.com/Rushhaabhhh/WallCraft",
+  },
+];
+
